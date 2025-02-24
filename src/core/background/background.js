903eb9ed5731d4d7ea31347037e3c84a9c20be10
itzrnvr <sanskar9797@gmail.com>
2025-02-24 17:22:23 +0530

    initial

---
+++ src/core/background/background.js
@@ -0,0 +1,16 @@
+// console.log("Background script loaded!");
+
+// Keep the service worker alive with an event listener
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("Extension installed/updated");
+});
+
+chrome.tabs.executeScript({
+    code: 'console.log("addd")'
+});
+
+
+  chrome.action.onClicked.addListener((tab) => {
+    // Optional: Add functionality when extension icon is clicked
+    console.log('Extension icon clicked');
+});
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  devtool: "cheap-module-source-map",
+  entry: {
+    background: './src/core/background/background.js',
+    content: './src/core/content/content.js',
+    popup: './src/core/popup/index.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+    publicPath: '/',
+  },
+
+  module: {
+    rules: [
+
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico|json)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name][ext]'
+        }
+      }
+    ],
+  },
+
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/manifest.json', to: 'manifest.json' },
+        { from: 'src/assets/icons', to: 'icons' },
+      ],
+    }),
+
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'core', 'popup', 'popup.html'),
+      filename: 'popup.html',
+      cache: false,
+    }),
+  ],
+
+  optimization: {
+    minimize: false,
+  },
+};
